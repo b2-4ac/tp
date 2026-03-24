d8bb1ebf327728afdd5532ece2dddc8aad49c39a
jianming <yeojm235@gmail.com>
2026-03-25 01:16:26 +0800

    Create and update storage components

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -25,9 +25,11 @@ import seedu.address.model.util.SampleDataUtil;
 import seedu.address.storage.AddressBookStorage;
 import seedu.address.storage.JsonAddressBookStorage;
 import seedu.address.storage.JsonUserPrefsStorage;
+import seedu.address.storage.JsonWorkoutLogBookStorage;
 import seedu.address.storage.Storage;
 import seedu.address.storage.StorageManager;
 import seedu.address.storage.UserPrefsStorage;
+import seedu.address.storage.WorkoutLogBookStorage;
 import seedu.address.ui.Ui;
 import seedu.address.ui.UiManager;
 
@@ -58,7 +60,8 @@ public class MainApp extends Application {
         UserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(config.getUserPrefsFilePath());
         UserPrefs userPrefs = initPrefs(userPrefsStorage);
         AddressBookStorage addressBookStorage = new JsonAddressBookStorage(userPrefs.getAddressBookFilePath());
-        storage = new StorageManager(addressBookStorage, userPrefsStorage);
+        WorkoutLogBookStorage workoutLogBookStorage = new JsonWorkoutLogBookStorage(userPrefs.getWorkoutLogBookFilePath());
+        storage = new StorageManager(addressBookStorage, userPrefsStorage, workoutLogBookStorage);
 
         model = initModelManager(storage, userPrefs);
 
---
+++ src/main/java/seedu/address/storage/JsonWorkoutLogBookStorage.java
@@ -0,0 +1,62 @@
+package seedu.address.storage;
+
+import static java.util.Objects.requireNonNull;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Optional;
+
+import seedu.address.commons.exceptions.DataLoadingException;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.commons.util.FileUtil;
+import seedu.address.commons.util.JsonUtil;
+import seedu.address.model.WorkoutLogBook;
+
+public class JsonWorkoutLogBookStorage implements WorkoutLogBookStorage {
+
+    private Path filePath;
+
+    public JsonWorkoutLogBookStorage(Path filePath) {
+        this.filePath = filePath;
+    }
+
+    @Override
+    public Path getWorkoutLogBookFilePath() {
+        return filePath;
+    };
+
+    @Override
+    public Optional<WorkoutLogBook> readWorkoutLogBook() throws DataLoadingException {
+        return readWorkoutLogBook(filePath);
+    };
+
+    @Override
+    public Optional<WorkoutLogBook> readWorkoutLogBook(Path filePath) throws DataLoadingException {
+        requireNonNull(filePath);
+
+        Optional<JsonSerializableWorkoutLogBook> jsonWorkoutLogBook = JsonUtil.readJsonFile(
+                filePath, JsonSerializableWorkoutLogBook.class);
+        if (!jsonWorkoutLogBook.isPresent()) {
+            return Optional.empty();
+        }
+
+        try {
+            return Optional.of(jsonWorkoutLogBook.get().toModelType());
+        } catch (IllegalValueException ive) {
+            throw new DataLoadingException(ive);
+        }
+    };
+
+    public void saveWorkoutLogBook(WorkoutLogBook workoutLogBook) throws IOException {
+        saveWorkoutLogBook(workoutLogBook, filePath);
+    };
+
+    public void saveWorkoutLogBook(WorkoutLogBook workoutLogBook, Path filePath) throws IOException {
+        requireNonNull(workoutLogBook);
+        requireNonNull(filePath);
+
+        FileUtil.createIfMissing(filePath);
+        JsonUtil.saveJsonFile(new JsonSerializableWorkoutLogBook(workoutLogBook), filePath);
+    };
+
+}
