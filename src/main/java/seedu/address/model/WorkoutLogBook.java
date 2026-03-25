f8b454ff44adb5720fbe4aea984ab1b91e5cd845
jianming <yeojm235@gmail.com>
2026-03-25 15:24:28 +0800

    Add test cases

--- src/main/java/seedu/address/model/WorkoutLogBook.java
+++ src/main/java/seedu/address/model/WorkoutLogBook.java
@@ -37,6 +37,8 @@ public class WorkoutLogBook {
      * @return True if the object exists, false otherwise
      */
     public boolean hasLog(WorkoutLog log) {
+        requireNonNull(log);
+
         return logs.contains(log);
     }
 
